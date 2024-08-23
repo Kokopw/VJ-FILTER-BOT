4a8415e45b0600d04f430cb55d7b52373d917b96
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2024-08-23 14:19:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -56,7 +56,7 @@ async def give_filter(client, message):
             try:
                 if settings.get('auto_ffilter'):
                     ai_search = True
-                    reply_msg = await message.reply_text(f"<b><i>Searching For {message.text} 🔍</i></b>")
+                    reply_msg = await message.reply_text(f"<b><i>Searching  {message.text} 🔍</i></b>")
                     await auto_filter(client, message.text, message, reply_msg, ai_search)
             except KeyError:
                 grpid = await active_connection(str(message.from_user.id))
@@ -64,7 +64,7 @@ async def give_filter(client, message):
                 settings = await get_settings(message.chat.id)
                 if settings.get('auto_ffilter'):
                     ai_search = True
-                    reply_msg = await message.reply_text(f"<b><i>Searching For {message.text} 🔍</i></b>")
+                    reply_msg = await message.reply_text(f"<b><i>Searching {message.text} 🔍</i></b>")
                     await auto_filter(client, message.text, message, reply_msg, ai_search)
     else: #a better logic to avoid repeated lines of code in auto_filter function
         search = message.text
