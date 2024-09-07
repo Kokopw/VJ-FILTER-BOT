29fdb35eadeafcfea314b4c87591da713cd30db2
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2024-09-07 17:55:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://single-isis-teekam-b5042e7c.koyeb.app/")
+URL = environ.get("URL", "https://tjbots.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
