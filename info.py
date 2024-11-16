b91943ba571baef2f780131b27efefc9714f5cf6
Kokopw <kmelon175@gmail.com>
2024-11-17 01:29:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://sleepy-maurizia-akonbots-8c26fd97.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
